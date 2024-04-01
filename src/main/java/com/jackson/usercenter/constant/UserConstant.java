979d815b9f5667e596a7e15e99d6ac28f980e387
JacksonZHR <1024060689@qq.com>
2024-04-01 22:12:43 +0800

    perf: 提取与用户相关的常量

---
+++ src/main/java/com/jackson/usercenter/constant/UserConstant.java
@@ -0,0 +1,21 @@
+package com.jackson.usercenter.constant;
+
+/**
+ * 用户常量
+ *
+ * @author JacksonZHR
+ */
+public interface UserConstant {
+
+    String USER_LOGIN_STATE = "userLoginState";
+
+    //--用户角色和权限-----------
+    /**
+     * 默认权限
+     */
+    int DEFAULT_ROLE = 0;
+    /**
+     * 管理员权限
+     */
+    int ADMIN_ROLE = 1;
+}
