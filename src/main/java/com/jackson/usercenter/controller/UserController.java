bd5ce3d9ba34335e8fedab00a39efd9b57d11fe4
JacksonZHR <1024060689@qq.com>
2024-09-06 16:29:00 +0800

    doc: 添加部分注释

--- src/main/java/com/jackson/usercenter/controller/UserController.java
+++ src/main/java/com/jackson/usercenter/controller/UserController.java
@@ -22,6 +22,11 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    /**
+     * 用户注册功能
+     * @param userRegisterRequest 用户注册请求体信息
+     * @return
+     */
     @PostMapping("/register")
     public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
         if (userRegisterRequest == null) return null;
@@ -34,6 +39,12 @@ public class UserController {
         return userService.userRegister(userAccount, userPassword, checkPassword);
     }
 
+    /**
+     * 用户登录
+     * @param userLoginRequest 用户登录请求体
+     * @param httpServletRequest http请求，set Session的状态
+     * @return
+     */
     @PostMapping("/login")
     public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest httpServletRequest) {
         if (userLoginRequest == null) return null;
@@ -45,6 +56,13 @@ public class UserController {
         return userService.userLogin(userAccount, userPassword, httpServletRequest);
     }
 
+    /**
+     * 查询用户
+     * 允许管理员能够模糊查询用户
+     * @param userAccount 要查询的用户
+     * @param httpServletRequest http请求
+     * @return
+     */
     @GetMapping("/search")
     public List<User> searchUser(String userAccount, HttpServletRequest httpServletRequest) {
         //仅管理员可查
@@ -54,6 +72,13 @@ public class UserController {
         return userService.searchUsers(userAccount);
     }
 
+    /**
+     * 删除用户
+     * 管理员能够根据id删除用户
+     * @param id 用户id
+     * @param httpServletRequest http请求体
+     * @return
+     */
     @PostMapping("/delete")
     public boolean deleteUser(@RequestBody long id, HttpServletRequest httpServletRequest) {
         //仅管理员可删除
